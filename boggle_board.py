1962121bb01ecf08cc44bd7ac5072eb92e80b022
kineticvenom <kalebvarnes@yahoo.com>
2022-06-03 06:34:05 -1000

    updated some code

--- boggle_board.py
+++ boggle_board.py
@@ -5,6 +5,8 @@ class BoggleBoard:
   def __init__(self):
     self.board = ["----"] * 4
 
+    
+
   def shake(self):
     letters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Qu','R','S','T','U','V','W','X','Y','Z']
     self.board = []
@@ -13,12 +15,11 @@ class BoggleBoard:
       for j in range(4):
         temp = random.choice(letters)
         if temp == "Qu":
-          row.append([f"{temp} "])
+          row.append(temp)
         else:
-          row.append([f'{temp}  '])
+          row.append(temp)
       self.board.append(row)
     print()
-    
   def reset(self):
     self.board = ["----"] * 4
     
@@ -32,4 +33,4 @@ for bog in my_bog.board:
   
 print()
 print()
-print(my_bog.board[0][3])
+print(my_bog.board[0][0])
